8c6394a6ee5ec7b0ca9bb74949ed16f2d788a8fc
e-boudhina <elyesboudhina@gmail.com>
2020-02-16 11:35:44 +0100

    Overriding default pagination and and adding cutom template & search queries

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,11 +8,19 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public  function index()
+    public  function index(Request $request)
     {
+        $search = $request->query('search');
+        if ($search)
+        {
+            $posts = Post::where('title','LIKE',"%{$search}%")->Simplepaginate(1);
+        }
+        else{
+            $posts = Post::Simplepaginate(2);
+        }
         return view('welcome')
             ->with('categories',Category::all())
             ->with('tags' ,Tag::all())
-            ->with('posts',Post::all());
+            ->with('posts',$posts);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                     <div class="col-md-8 col-xl-9">
                         <div class="row gap-y">
 
-                            @foreach($posts as $post)
+                            @forelse($posts as $post)
                                 <div class="col-md-6">
                                     <div class="card border hover-shadow-6 mb-6 d-block">
                                         <a href="{{route('blog.show',$post->id)}}"><img class="card-img-top" src="{{ asset('storage/'.$post->image) }}" alt="Card image cap"></a>
@@ -45,17 +45,23 @@
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
+                                @empty
+                            <p class="text-center">No result found fore query <strong>{{ request()->query('search') }}</strong></p>
+                            @endforelse
 
 
                         </div>
 
 
-                        <nav class="flexbox mt-30">
-                            <a class="btn btn-white disabled"><i class="ti-arrow-left fs-9 mr-4"></i> Newer</a>
-                            <a class="btn btn-white" href="#">Older <i class="ti-arrow-right fs-9 ml-4"></i></a>
-                        </nav>
-                    </div>
+{{--                        <nav class="flexbox mt-30">--}}
+{{--                            <a class="btn btn-white disabled"><i class="ti-arrow-left fs-9 mr-4"></i> Newer</a>--}}
+{{--                            <a class="btn btn-white" href="#">Older <i class="ti-arrow-right fs-9 ml-4"></i></a>--}}
+{{--                        </nav>--}}
+
+{{--                        If you like to keep the search query request you must tie it to the pagination or else you'll lose it every time you move from one page to the other --}}
+                        {{$posts->appends(['search' => request()->query('search')])->links()}}
+
+             </div>
 
 
 
@@ -63,8 +69,8 @@
                         <div class="sidebar px-4 py-md-0">
 
                             <h6 class="sidebar-title">Search</h6>
-                            <form class="input-group" target="#" method="GET">
-                                <input type="text" class="form-control" name="s" placeholder="Search">
+                            <form class="input-group"action="{{route('welcome')}}" method="GET">
+                                <input type="text" class="form-control" name="search" placeholder="Search" value="{{ request()->query('search') }}">
                                 <div class="input-group-addon">
                                     <span class="input-group-text"><i class="ti-search"></i></span>
                                 </div>
